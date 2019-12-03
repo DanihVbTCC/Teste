8b2049c7c585c2bbbb86eb4da7a5269470fea77a
Daniel Velluto Bento <danielzinhobento11@gmail.com>
2019-12-03 08:36:18 -0300

    fim

--- src/app/calculadora-b/calculadora-b.page.ts
+++ src/app/calculadora-b/calculadora-b.page.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ToastController } from '@ionic/angular';
-import { PostProvider } from '../../providers/post-provider';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 import { Storage } from '@ionic/Storage';
 import { AlertController } from '@ionic/angular';
 import { MbscFormOptions } from '@mobiscroll/angular';
+import { CalculadoraIPage } from '../calculadora-i/calculadora-i.page';
 
 
 @Component({
@@ -17,33 +17,30 @@ export class CalculadoraBPage implements OnInit {
   formSettings: MbscFormOptions = {
     lang: 'pt-BR',
     theme: 'ios'
-};
+  };
 
-  homens: number;
-  mulheres: number;
-  adolescentes: number;
-  criancas: number;
-  horas: number;
-  dados: any=[];
-  anggota: any;
+  homens: number=0;
+  mulheres: number=0;
+  adolescentes: number=0;
+  criancas: number=0;
+  horas: number=0;
+  dados: any;
 
   constructor(
     private router: Router,
-    private postPvdr: PostProvider,
     private storage: Storage,
     public toastCtrl: ToastController,
-    private actRoute: ActivatedRoute,
     public alertController: AlertController
  ) { }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   formCalculadorac(){
     this.dados = [this.homens, this.mulheres, this.adolescentes, this.criancas];
     this.storage.set('session_dados', this.dados);
-    this.router.navigate(['/calculadora-c']);
       console.log(this.dados);
+    this.router.navigate(['/calculadora-c']);
     
   }
+
 }
